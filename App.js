149d5d4c73447420b5b3cb80ea57881d9f62fd71
TeguhVm <teguhvisyaifulmujab@student.uir.ac.id>
2024-12-25 15:03:05 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text style={styles.text}>TEGUH VISYAIFUL MUJAB,5C!</Text>
+
+    
+      <View style={styles.boxContainer}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#D3D3D3', 
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 20, 
+  },
+  boxContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    width: '80%', 
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
   },
 });
+
+export default App;
